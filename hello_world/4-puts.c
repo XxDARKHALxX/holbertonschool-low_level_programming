4d3d68cfcd2baa9950a1586897b5d3b9b22f2c2c
XxDARKHALxX <jonathanperez604@gmail.com>
2022-10-06 14:20:50 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - write a line or string to the output
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
