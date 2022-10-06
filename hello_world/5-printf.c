627e8f793bc7e2e625ae017793216d7f12115cc2
XxDARKHALxX <jonathanperez604@gmail.com>
2022-10-06 14:21:11 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - sends a formatted string to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
