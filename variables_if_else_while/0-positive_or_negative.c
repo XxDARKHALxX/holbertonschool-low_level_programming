7bc234f1f02574ac442a1438cefd8e7edbd733f1
XxDARKHALxX <jonathanperez604@gmail.com>
2022-10-06 18:50:00 +0000

    positive an negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - output negative or positive
+ *
+ * Return: always o (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n");
+	else if (n < 0)
+		primtf("%d is negative\n");
+	else
+		printf("%d is zero\n");
+	return (0);
+}
